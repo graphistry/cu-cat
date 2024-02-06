daf9bdef6e971a1833a73f69343abc490ccdf9e3
Daniel <dcolinmorgan@gmail.com>
2024-02-06 14:50:53 +0800

    lint

--- cu_cat/__init__.py
+++ cu_cat/__init__.py
@@ -24,10 +24,6 @@ from ._version import get_versions
 __version__ = get_versions()["version"]
 del get_versions
 
-
-__version__ = get_versions()["version"]
-del get_versions
-
 __all__ = [
     "DatetimeEncoder",
     "GapEncoder",
